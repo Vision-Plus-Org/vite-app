b5595cdc28dd80c867e28bf22f7d2ff49134b0bb
Vision Hung <scps950613@gmail.com>
2023-04-21 18:11:34 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h2-button
+      icon="add"
+    >
+      Click me
+    </h2-button>
+    <h2-icon
+      filename="add_solid"
+      button
+      color="orange"
+    />
+    <H2SingleDatePicker
+      v-model="date"
+      footnote="footnote text"
+    />
+    <H2DateRangePicker
+      v-model="dateRange"
+      footnote="footnote text"
+    />
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import { H2Button, H2Icon, H2SingleDatePicker, H2DateRangePicker } from '@vision-plus-org/vue-ui';
+
+const date = ref('2023-01-01')
+const dateRange = ref({ from: '', to: ''})
+</script>
